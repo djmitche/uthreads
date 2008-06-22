9fc39a3d269dd15dbc660bb019fb73ec3598f716
djmitche <djmitche@226abe4b-112f-0410-b514-dff0a6313733>
2008-06-22 17:06:45 +0000

    correct project name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (2, 5):
   print "Python 2.5 or higher is required."
   sys.exit(1)
 
-setup(name='uthread',
+setup(name='uthreads',
       version='1.0a3',
       author='Dustin J. Mitchell',
       author_email='dustin@cs.uchicago.edu',
